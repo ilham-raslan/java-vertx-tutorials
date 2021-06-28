61ab9cd52b82c3301eb630c705a41ef11ce27084
ilham-raslan <ilhamraslan@yahoo.com>
2021-06-28 11:56:41 +0100

    Completed vertx 4 course

---
+++ vertx-websockets/src/main/java/com/ilham/github/vertx_websockets/MainVerticle.java
@@ -0,0 +1,24 @@
+package com.ilham.github.vertx_websockets;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainVerticle extends AbstractVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().webSocketHandler(new WebSocketHandler(vertx))
+            .listen(8900, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        LOG.info("HTTP server started on port 8900");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
---
+++ vertx-websockets/src/test/java/com/ilham/github/vertx_websockets/TestMainVerticle.java
@@ -0,0 +1,80 @@
+package com.ilham.github.vertx_websockets;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.WebSocketConnectOptions;
+import io.vertx.junit5.Timeout;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(VertxExtension.class)
+public class TestMainVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(TestMainVerticle.class);
+  public static final int EXPECTED_MESSAGES = 5;
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext context) {
+    vertx.deployVerticle(new MainVerticle(), context.succeeding(id -> context.completeNow()));
+  }
+
+  @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
+  @Test
+  void can_connect_to_web_socket_server(Vertx vertx, VertxTestContext context) throws Throwable {
+    var client = vertx.createHttpClient();
+
+    client.webSocket(8900, "localhost", WebSocketHandler.PATH)
+      .onFailure(context::failNow)
+      .onComplete(context.succeeding(ws -> {
+        ws.handler(data -> {
+          final var receivedData = data.toString();
+          LOG.debug("Received message: {}", receivedData);
+          assertEquals("Connected!", data.toString());
+          client.close();
+          context.completeNow();
+        });
+      })
+      );
+  }
+
+  @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
+  @Test
+  void can_receive_multiple_messages(Vertx vertx, VertxTestContext context) throws Throwable {
+    var client = vertx.createHttpClient();
+
+    final AtomicInteger counter = new AtomicInteger(0);
+
+    client.webSocket(new WebSocketConnectOptions()
+            .setHost("localhost")
+            .setPort(8900)
+            .setURI(WebSocketHandler.PATH)
+    )
+            .onFailure(context::failNow)
+            .onComplete(context.succeeding(ws -> {
+              ws.handler(data -> {
+                final var receivedData = data.toString();
+                LOG.debug("Received message: {}", receivedData);
+                var currentValue = counter.getAndIncrement();
+                if (currentValue > EXPECTED_MESSAGES) {
+                  client.close();
+                  context.completeNow();
+                }
+                else {
+                  LOG.debug("Not enough messages yet... ({}/{})", currentValue, EXPECTED_MESSAGES);
+                }
+                client.close();
+                context.completeNow();
+              });
+            }));
+  }
+}
